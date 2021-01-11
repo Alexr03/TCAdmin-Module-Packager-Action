63df2f3b5940b3d60cf38feb99a905c4150cad33
Alex Redding <Alexredding180@gmail.com>
2021-01-11 23:32:26 +0000

    Initial

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+﻿FROM mcr.microsoft.com/dotnet/sdk:5.0 AS build
+WORKDIR /src
+
+# copy everything else and build app
+COPY . .
+RUN dotnet build -c release -o /app
+RUN dotnet publish -c release -o /app
+
+# final stage/image
+FROM mcr.microsoft.com/dotnet/runtime:5.0
+WORKDIR /app
+COPY --from=build /app ./
+ENTRYPOINT ["dotnet", "DeployToInvisionAction.dll"]
---
+++ TCAdmin-Module-Packager-Action/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using Serilog;
+
+namespace TCAdmin_Module_Packager_Action
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SetupEnvironment();
+            Log.Information("Hello World!");
+            Log.Information("Directory: " + Directory.GetCurrentDirectory());
+        }
+
+        private static void SetupEnvironment()
+        {
+            DotNetEnv.Env.TraversePath().Load();
+            CreateLogger();
+            
+        }
+
+        private static void CreateLogger()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .MinimumLevel.Debug()
+                .CreateLogger();
+        }
+    }
+}
